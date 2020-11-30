8bf02d30fbd98796b285f544cb079fa867620d10
Supachai Phobpimai <611463023@crru.ac.th>
2020-11-30 09:33:40 +0700

    add comment to connection

--- echo_server.py
+++ echo_server.py
@@ -24,5 +24,5 @@ while True:
                 server_wait = False
  
     finally:
-        # Clean up the connection
+        # Clean up the connection 5555
         connection.close() #ปิดการเชื่อมต่อ
